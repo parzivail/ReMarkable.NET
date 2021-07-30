d28929e29749c44ffabea940d74fb7736618c19d
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2021-07-30 22:25:05 +0100

    This enables remarkable.net to correctly use rm2fb via direct invocation.
    It also abstracts version information into a new enum

--- ReMarkable.NET/Unix/Driver/InputDevices.cs
+++ ReMarkable.NET/Unix/Driver/InputDevices.cs
@@ -33,33 +33,31 @@ namespace ReMarkable.NET.Unix.Driver
         /// </summary>
         static InputDevices()
         {
-#if DEBUG
-            // Load emulated input devices
-            var deviceContainer = Type.GetType("RmEmulator.EmulatedDevices, RmEmulator");
-            if (deviceContainer != null)
+            switch (DeviceType.GetDevice())
             {
-                deviceContainer.ReadStaticField("PhysicalButtons", out PhysicalButtons);
-                deviceContainer.ReadStaticField("Touchscreen", out Touchscreen);
-                deviceContainer.ReadStaticField("Digitizer", out Digitizer);
-
-                return;
-            }
-#endif
-            // Load hardware input devices
-            var deviceMap = DeviceUtils.GetInputDeviceEventHandlers();
-            
-            if (deviceMap.ContainsKey("30370000.snvs:snvs-powerkey"))
-            {
-                // rM2
-                PhysicalButtons = new HardwarePhysicalButtonDriver(deviceMap["30370000.snvs:snvs-powerkey"]);
-                Touchscreen = new HardwareTouchscreenDriver(deviceMap["cyttsp5_mt"], 767, 1023, 32);
-                Digitizer = new HardwareDigitizerDriver(deviceMap["Wacom I2C Digitizer"], 20967, 15725);
-            }
-            else
-            {
-                PhysicalButtons = new HardwarePhysicalButtonDriver(deviceMap["gpio-keys"]);
-                Touchscreen = new HardwareTouchscreenDriver(deviceMap["cyttsp5_mt"], 767, 1023, 32);
-                Digitizer = new HardwareDigitizerDriver(deviceMap["Wacom I2C Digitizer"], 20967, 15725);
+                case Device.Emulator:
+                    // Load emulated input devices
+                    var deviceContainer = Type.GetType("RmEmulator.EmulatedDevices, RmEmulator");
+                    if (deviceContainer != null)
+                    {
+                        deviceContainer.ReadStaticField("PhysicalButtons", out PhysicalButtons);
+                        deviceContainer.ReadStaticField("Touchscreen", out Touchscreen);
+                        deviceContainer.ReadStaticField("Digitizer", out Digitizer);
+                    }
+                    break;
+                case Device.RM1:
+                    var rm1DeviceMap = DeviceUtils.GetInputDeviceEventHandlers();
+                    PhysicalButtons = new HardwarePhysicalButtonDriver(rm1DeviceMap["gpio-keys"]);
+                    Touchscreen = new HardwareTouchscreenDriver(rm1DeviceMap["cyttsp5_mt"], 767, 1023, 32);
+                    Digitizer = new HardwareDigitizerDriver(rm1DeviceMap["Wacom I2C Digitizer"], 20967, 15725);
+                    break;
+                case Device.RM2:
+                    // rM2
+                    var rm2DeviceMap = DeviceUtils.GetInputDeviceEventHandlers();
+                    PhysicalButtons = new HardwarePhysicalButtonDriver(rm2DeviceMap["30370000.snvs:snvs-powerkey"]);
+                    Touchscreen = new HardwareTouchscreenDriver(rm2DeviceMap["pt_mt"], 767, 1023, 32);
+                    Digitizer = new HardwareDigitizerDriver(rm2DeviceMap["Wacom I2C Digitizer"], 20967, 15725);
+                    break;
             }
         }
     }
---
+++ ReMarkable.NET/Util/DeviceType.cs
@@ -0,0 +1,61 @@
+﻿using ReMarkable.NET.Unix.Driver;
+using System.Runtime.InteropServices;
+
+namespace ReMarkable.NET.Util
+{
+    /// <summary>
+    /// Possible Remarkable or emulated device types
+    /// </summary>
+    public enum Device
+    {
+        Emulator,
+        RM1,
+        RM2
+    }
+
+    public class DeviceType
+    {
+        private static Device ?result = null;
+
+        /// <summary>
+        /// Gets the device that in currently in use
+        /// </summary>
+        /// <returns>DeviceType</returns>
+        public static Device GetDevice()
+        {
+            if (result != null)
+            {
+                return result.Value;
+            }
+
+            var ret = Device.Emulator;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                ret = Device.Emulator;
+            }
+            else
+            {
+                try
+                {
+                    var deviceMap = DeviceUtils.GetInputDeviceEventHandlers();
+
+                    if (deviceMap.ContainsKey("30370000.snvs:snvs-powerkey")) // rm2 has a power button
+                    {
+                        ret = Device.RM2;
+                    }
+                    else if (deviceMap.ContainsKey("cyttsp5_mt")) // rm1 touch screen driver
+                    {
+                        ret = Device.RM1;
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            result = ret;
+            return ret;
+        }
+    }
+}
