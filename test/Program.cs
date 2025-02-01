9061da74a19c7b1aa323b1086d7e2fb16b7d9a82
Petr Carda <p.carda@seznam.cz>
2025-02-01 23:51:41 +0100

    project settings

---
+++ test/Program.cs
@@ -0,0 +1,16 @@
+﻿using ReMarkable.NET.Unix.Driver;
+using ReMarkable.NET.Util;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var devices = DeviceUtils.GetInputDeviceEventHandlers();
+        foreach (var kv in devices)
+        {
+            Console.Out.WriteLine($"{kv.Key} = {kv.Value}");
+        }
+
+        Console.Out.WriteLine("Detected: " + DeviceType.GetDevice());
+    }
+}
